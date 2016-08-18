06433f4567d76edb5decb2e7f9ad5af2bc9ce3c4
bloesch <bloeschm@ethz.ch>
2016-08-18 13:23:56 +0200

    added chrono functionality

--- include/generalized_information_filter/common.hpp
+++ include/generalized_information_filter/common.hpp
@@ -12,23 +12,37 @@
 #include <iostream>
 #include <memory>
 #include "kindr/Core"
+#include <chrono>
 
-typedef kindr::RotationQuaternionPD QPD;
-typedef kindr::RotationMatrixPD MPD;
-typedef Eigen::Vector3d V3D;
-typedef Eigen::Matrix3d M3D;
-typedef Eigen::VectorXd VXD;
-typedef Eigen::MatrixXd MXD;
-inline M3D gSM(const V3D& vec){
-  return kindr::getSkewMatrixFromVector(vec);
-}
+namespace GIF{
 
-static void enforceSymmetry(MXD& mat){
-  mat = 0.5*(mat+mat.transpose()).eval();
-}
+  typedef kindr::RotationQuaternionPD QPD;
+  typedef kindr::RotationMatrixPD MPD;
+  typedef Eigen::Vector3d V3D;
+  typedef Eigen::Matrix3d M3D;
+  typedef Eigen::VectorXd VXD;
+  typedef Eigen::MatrixXd MXD;
+  typedef std::chrono::high_resolution_clock Clock;
+  typedef Clock::time_point TimePoint;
+  typedef Clock::duration Duration;
+  inline Duration fromSec(const double& sec){
+    return std::chrono::duration_cast<Duration>(std::chrono::duration<double>(sec));
+  }
+  inline double toSec(const Duration& duration){
+    return std::chrono::duration_cast<std::chrono::duration<double>>(duration).count();
+  }
+  inline M3D gSM(const V3D& vec){
+    return kindr::getSkewMatrixFromVector(vec);
+  }
 
-inline M3D Lmat (const V3D& a) {
-  return kindr::getJacobianOfExponentialMap(a);
+  static void enforceSymmetry(MXD& mat){
+    mat = 0.5*(mat+mat.transpose()).eval();
+  }
+
+  inline M3D Lmat (const V3D& a) {
+    return kindr::getJacobianOfExponentialMap(a);
+  }
+
 }
 
 #endif /* GIF_COMMON_HPP_ */
