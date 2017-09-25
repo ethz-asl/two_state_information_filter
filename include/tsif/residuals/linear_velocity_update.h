e0abe263c95cc21d219e4d59fd15323d33ecf6b8
tree <trfabian@student.ethz.ch>
2017-09-25 17:16:36 +0200

    added linear velocity upate

---
+++ include/tsif/residuals/linear_velocity_update.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "tsif/utils/common.h"
+#include "tsif/residual.h"
+
+namespace tsif{
+
+class MeasLvel: public ElementVector<Element<Vec3,0>>{
+ public:
+  MeasLvel(): ElementVector<Element<Vec3,0>>(Vec3(0,0,0)){}
+  MeasLvel(const Vec3& vel): ElementVector<Element<Vec3,0>>(vel){}
+  const Vec3& GetMeas() const{
+    return Get<0>();
+  }
+  Vec3& GetMeas(){
+    return Get<0>();
+  }
+};
+
+template<int Y, int B_V_IB>
+using LinearVelocityUpdateBase = Residual<ElementVector<Element<Vec3,Y>>,
+                                          ElementVector<>,
+                                          ElementVector<Element<Vec3,B_V_IB>>,
+                                          MeasLvel>;
+
+template<int B_V_IB>
+class LinearVelocityUpdate: public LinearVelocityUpdateBase<0,B_V_IB>{
+ public:
+  typedef LinearVelocityUpdateBase<0,B_V_IB> Base;
+  using Base::dt_;
+  using Base::w_;
+  using Base::meas_;
+  typedef typename Base::Output Output;
+  typedef typename Base::Previous Previous;
+  typedef typename Base::Current Current;
+  LinearVelocityUpdate(): Base(false,false,true){}
+  int EvalRes(typename Output::Ref out, const typename Previous::CRef pre, const typename Current::CRef cur){
+    out.template Get<0>() = cur.template Get<B_V_IB>() - meas_->GetMeas();
+    return 0;
+  }
+  int JacPre(MatRefX J, const typename Previous::CRef pre, const typename Current::CRef cur){
+    return 0;
+  }
+  int JacCur(MatRefX J, const typename Previous::CRef pre, const typename Current::CRef cur){
+    J.block<3,3>(Output::Start(B_V_IB),cur.Start(B_V_IB)) = Mat3::Identity();
+    return 0;
+  }
+  double GetWeight(){
+    return w_;
+  }
+};
+
+} // namespace tsif
