aa2c91f628d915a0b0702bf8cf60eff334cf5e19
tree <trfabian@student.ethz.ch>
2017-10-17 17:20:42 +0200

    added imu offset as filter state in alternative position findif and landmark in base residuals

--- include/tsif/residuals/landmark_in_base_findif.h
+++ include/tsif/residuals/landmark_in_base_findif.h
@@ -17,21 +17,22 @@ class LandmarkInBaseFlagMeasurement : public ElementVector<Element<bool,0>>{
 
 //shortcut to make the templating more convenient
 template<int Y,//contact point innovation
-         int B_V_IB,//linear velocity base to odom in base frame
+         int B_V_IM,//linear velocity base to odom in base frame
          int B_OMEGA_IB,//angular velocity base to odom in base
-         int B_P>//landmark in base frame
+         int B_P,//landmark in base frame
+         int B_R_BM>
 using LandmarkInBaseFindifBase = Residual<ElementVector<Element<Vec3,Y>>,//innovation
-                                    ElementVector<Element<Vec3,B_V_IB>,Element<Vec3,B_OMEGA_IB>,Element<Vec3,B_P>>,//previous state
+                                    ElementVector<Element<Vec3,B_V_IM>,Element<Vec3,B_OMEGA_IB>,Element<Vec3,B_P>,Element<Vec3,B_R_BM>>,//previous state
                                     ElementVector<Element<Vec3,B_P>>,//current state
                                     LandmarkInBaseFlagMeasurement>;//measurement
 
 //prediction residual comparing current landmark position with 
-template<int B_V_IB,int B_OMEGA_IB,int B_P>
-class LandmarkInBaseFindif : public LandmarkInBaseFindifBase<0,B_V_IB,B_OMEGA_IB,B_P> {
+template<int B_V_IM,int B_OMEGA_IB,int B_P, int B_R_BM>
+class LandmarkInBaseFindif : public LandmarkInBaseFindifBase<0,B_V_IM,B_OMEGA_IB,B_P,B_R_BM> {
  public:
   //typedef and types from base for convenience
   //base type
-  typedef LandmarkInBaseFindifBase<0,B_V_IB,B_OMEGA_IB,B_P> Base;
+  typedef LandmarkInBaseFindifBase<0,B_V_IM,B_OMEGA_IB,B_P,B_R_BM> Base;
   //innovation vector type
   using typename Base::Output;
   //previous state vector type
@@ -55,7 +56,7 @@ class LandmarkInBaseFindif : public LandmarkInBaseFindifBase<0,B_V_IB,B_OMEGA_IB
    //compare measured contact points to filter state
 	 out. template Get<0>()=cur.template Get<B_P>()
                           -(Mat3::Identity()-SSM(dt_*pre.template Get<B_OMEGA_IB>()))*pre.template Get<B_P>()
-                          +dt_*pre.template Get<B_V_IB>();
+                          +dt_*(pre.template Get<B_V_IM>()-SSM(pre.template Get<B_OMEGA_IB>())*pre.template Get<B_R_BM>());
    return 0;
   }
   //function evaluating the jacobian of the the innovation wrt the previous state
@@ -64,9 +65,9 @@ class LandmarkInBaseFindif : public LandmarkInBaseFindifBase<0,B_V_IB,B_OMEGA_IB
    J.setZero();
    //set nonzero blocks of the jacobian
    //derivative of innovation wrt linear velocity
-   this->template SetJacPre<0,B_V_IB>(J,pre,dt_*Mat3::Identity());
+   this->template SetJacPre<0,B_V_IM>(J,pre,dt_*Mat3::Identity());
    //derivative of innovation wrt angular velocity
-   this->template SetJacPre<0,B_OMEGA_IB>(J,pre,-SSM(dt_*pre.template Get<B_P>()));
+   this->template SetJacPre<0,B_OMEGA_IB>(J,pre,-SSM(dt_*pre.template Get<B_P>())+SSM(dt_*pre.template Get<B_R_BM>()));
    //derivative of innovation wrt landmark
    this->template SetJacPre<0,B_P>(J,pre,SSM(dt_*pre.template Get<B_OMEGA_IB>())-Mat3::Identity());
    return 0;    
--- include/tsif/residuals/position_findif_offset.h
+++ include/tsif/residuals/position_findif_offset.h
@@ -6,16 +6,16 @@
 
 namespace tsif{
 
-template<int OUT_POS, int STA_POS, int STA_VEL, int STA_ATT, int STA_ROR>
+template<int OUT_POS, int STA_POS, int STA_VEL, int STA_ATT, int STA_ROR, int CST_OFF>
 using PositionFindifBase = Residual<ElementVector<Element<Vec3,OUT_POS>>,
-                                 ElementVector<Element<Vec3,STA_POS>,Element<Vec3,STA_VEL>,Element<Quat,STA_ATT>,Element<Vec3,STA_ROR>>,
+                                 ElementVector<Element<Vec3,STA_POS>,Element<Vec3,STA_VEL>,Element<Quat,STA_ATT>,Element<Vec3,STA_ROR>,Element<Vec3,CST_OFF>>,
                                  ElementVector<Element<Vec3,STA_POS>>,
                                  MeasEmpty>;
 
-template<int OUT_POS, int STA_POS, int STA_VEL, int STA_ATT, int STA_ROR>
-class PositionFindif: public PositionFindifBase<OUT_POS,STA_POS,STA_VEL,STA_ATT,STA_ROR>{
+template<int OUT_POS, int STA_POS, int STA_VEL, int STA_ATT, int STA_ROR,int CST_OFF>
+class PositionFindif: public PositionFindifBase<OUT_POS,STA_POS,STA_VEL,STA_ATT,STA_ROR,CST_OFF>{
  public:
-  typedef PositionFindifBase<OUT_POS,STA_POS,STA_VEL,STA_ATT,STA_ROR> Base;
+  typedef PositionFindifBase<OUT_POS,STA_POS,STA_VEL,STA_ATT,STA_ROR,CST_OFF> Base;
   using Base::dt_;
   using Base::w_;
   typedef typename Base::Output Output;
@@ -24,15 +24,15 @@ class PositionFindif: public PositionFindifBase<OUT_POS,STA_POS,STA_VEL,STA_ATT,
   PositionFindif(): Base(true,true,true){}
   int EvalRes(typename Output::Ref out, const typename Previous::CRef pre, const typename Current::CRef cur){
     out.template Get<OUT_POS>() = cur.template Get<STA_POS>() - pre.template Get<STA_POS>()
-      - dt_*pre.template Get<STA_ATT>().toRotationMatrix()*(pre.template Get<STA_VEL>()-SSM(pre.template Get<STA_ROR>())*Vec3(0.0245, -0.04255, 0.1857));
+      - dt_*pre.template Get<STA_ATT>().toRotationMatrix()*(pre.template Get<STA_VEL>()-SSM(pre.template Get<STA_ROR>())*pre.template Get<CST_OFF>());
     return 0;
   }
   int JacPre(MatRefX J, const typename Previous::CRef pre, const typename Current::CRef cur){
     J.block<3,3>(Output::Start(OUT_POS),pre.Start(STA_POS)) = -Mat3::Identity();
     J.block<3,3>(Output::Start(OUT_POS),pre.Start(STA_VEL)) = -pre.template Get<STA_ATT>().toRotationMatrix()*dt_;
     J.block<3,3>(Output::Start(OUT_POS),pre.Start(STA_ATT)) = SSM(dt_*pre.template Get<STA_ATT>().toRotationMatrix()*
-                                                                  (pre.template Get<STA_VEL>()-SSM(pre.template Get<STA_ROR>())*Vec3(0.0245, -0.04255, 0.1857)));
-    J.block<3,3>(Output::Start(OUT_POS),pre.Start(STA_ROR)) = -dt_*pre.template Get<STA_ATT>().toRotationMatrix()*SSM(Vec3(0.0245, -0.04255, 0.1857));
+                                                                  (pre.template Get<STA_VEL>()-SSM(pre.template Get<STA_ROR>())*pre.template Get<CST_OFF>()));
+    J.block<3,3>(Output::Start(OUT_POS),pre.Start(STA_ROR)) = -dt_*pre.template Get<STA_ATT>().toRotationMatrix()*SSM(pre.template Get<CST_OFF>());
     return 0;
   }
   int JacCur(MatRefX J, const typename Previous::CRef pre, const typename Current::CRef cur){
